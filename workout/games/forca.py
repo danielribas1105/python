72796efce604a73d8786552c60140244dfeed63d
Daniel Ribas <danielribas1105@gmail.com>
2022-08-20 22:00:10 -0300

    update-220820

--- workout/games/forca.py
+++ workout/games/forca.py
@@ -1,7 +1,32 @@
+from time import sleep
+
 def jogar():
     print('*'*19)
     print('** JOGO DA FORCA **')
     print('*'*19)
+
+    palavra_secreta='banana'
+    enforcou=False
+    acertou=False
+    i=0
+    cont_letra=0
+
+    print('Adivinhe a palavra se for capaz!')
+    sleep(1)
+    print('Palavra com {} letras'.format(len(palavra_secreta)))
+
+    while(not enforcou and not acertou):
+        chute = input('Digite uma letra: ').lower().strip()
+        
+        for letra in palavra_secreta:
+            if (chute==letra):
+                cont_letra += 1
+                print(letra)
+                print(palavra_secreta.find(chute))
+            i += 1
+            enforcou=True
+        print('A palavra secreta possui {} letra(s) {}'.format(cont_letra,chute))
+
 
 if(__name__ == '__main__'):
     jogar()
